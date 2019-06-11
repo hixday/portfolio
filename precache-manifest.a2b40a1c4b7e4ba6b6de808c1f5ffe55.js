79e0f1608dcbb4a250a66ebb217bdb31833bb83f
samuel faucher <sam.faucher938@gmail.com>
2019-06-10 21:08:44 -0400

    Updates

---
+++ precache-manifest.a2b40a1c4b7e4ba6b6de808c1f5ffe55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2381c4211bf1787e1f0630f9d9829cf5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "09f479c16204966cb8fe",
+    "url": "/portfolio/static/css/main.42e45e61.chunk.css"
+  },
+  {
+    "revision": "b13a083457cb0eff7d3c",
+    "url": "/portfolio/static/js/2.59557660.chunk.js"
+  },
+  {
+    "revision": "09f479c16204966cb8fe",
+    "url": "/portfolio/static/js/main.3a6eeaf5.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "6c48d04ba3633cedcea19c014a1679ea",
+    "url": "/portfolio/static/media/creation-white.6c48d04b.svg"
+  },
+  {
+    "revision": "7633092603e1959d0acaf5b0e12b8cdc",
+    "url": "/portfolio/static/media/creation.76330926.svg"
+  },
+  {
+    "revision": "caee433e425bc072b54fb78096484acf",
+    "url": "/portfolio/static/media/loupe.caee433e.svg"
+  },
+  {
+    "revision": "3497d5532a2fb3c152c014762bf7a5a2",
+    "url": "/portfolio/static/media/responsive-white.3497d553.svg"
+  },
+  {
+    "revision": "d4e8c74e8f95664d1bd8c710e2fcc6d1",
+    "url": "/portfolio/static/media/sam.d4e8c74e.jpg"
+  },
+  {
+    "revision": "4fc5c829e66f3a2eac04c18559bb5f91",
+    "url": "/portfolio/static/media/speedometer-white.4fc5c829.svg"
+  },
+  {
+    "revision": "8278fa9c9f84c63dbbb77be368c1279c",
+    "url": "/portfolio/static/media/ux.8278fa9c.svg"
+  }
+]);
